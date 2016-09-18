7403f9ce9813f5802923ed363178994da44f8cd5
Michael Ashby <ashby.michael@gmail.com>
2016-09-18 16:30:51 -0700

    enable uploading popos through csv

--- app/controllers/popos_controller.rb
+++ app/controllers/popos_controller.rb
@@ -1,3 +1,7 @@
 class PoposController < ApplicationController
-
+  def create
+    Popo.import(params[:file])
+    flash[:success] = "Popos uploaded"
+    redirect_to root_url
+  end
 end
--- app/models/popo.rb
+++ app/models/popo.rb
@@ -1,2 +1,16 @@
 class Popo < ApplicationRecord
+  def self.import(file)
+    CSV.foreach(file.tempfile, headers: true) do |row|
+      popos = Popo.new(name: row[0],
+                       lat: row[2],
+                       lng: row[3],
+                       restrooms: row[7],
+                       address: row[1],
+                       image: row[5],
+                       description: row[8],
+                       hours: row[10],
+                       location: row[9])
+      popos.save if popos.valid?
+    end
+  end
 end
